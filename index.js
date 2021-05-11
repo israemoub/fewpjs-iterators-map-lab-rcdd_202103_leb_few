69088e9ab492d4029933aca035ab3976b6882eb2
Israe Moubayed <israetarek@yahoo.com>
2021-05-11 17:21:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,8 +16,31 @@ const titleCased = () => {
 */
 
 
-const capatalize = tutorials.map(function (tutorial){
-  for (let i = 0; i < tutorials.length; i ++) {
-    return tutorials[i].split(' ').map(w => w.charAt(0).toUpperCase() + w.substring(1)).join(' ');
-  }
-})
+const tutorials = [
+  'what does the this keyword mean?',
+  'What is the Contutorialuctor OO pattern?',
+  'implementing Blockchain Web API',
+  'The Test Driven Development Workflow',
+  'What is NaN and how Can we Check for it',
+  'What is the difference between stopPropagation and preventDefault?',
+  'Immutable State and Pure Functions',
+  'what is the difference between == and ===?',
+  'what is the difference between event capturing and bubbling?',
+  'what is JSONP?'
+];
+
+
+function titleCase(str) {
+  str = str.split(' ');
+  if(str !== "OO" || str !== "JSONP"){
+  for (var i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+  }}
+  return str.join(' ');
+}
+
+const titleCased = () => tutorials.map(tutorial => titleCase(tutorial));
+  
+
+titleCased();
+
