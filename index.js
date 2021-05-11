d28fd600b1a3902788fe8d2075546e0fe5826974
Israe Moubayed <israetarek@yahoo.com>
2021-05-11 17:21:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,13 +34,12 @@ function titleCase(str) {
   str = str.split(' ');
   if(str !== "OO" || str !== "JSONP"){
   for (var i = 0; i < str.length; i++) {
-        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
   }}
   return str.join(' ');
 }
 
 const titleCased = () => tutorials.map(tutorial => titleCase(tutorial));
-  
+
 
 titleCased();
-
